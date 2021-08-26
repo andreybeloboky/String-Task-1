06d5739565c8ec0fe3e9554e24675aaad3d24372
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-08-26 18:30:24 +0300

    add string i like java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String str = new String();
+    str = "I like Java!!!";
+    System.out.println(str.length());
+    System.out.println(str.charAt(13));
     }
 }
